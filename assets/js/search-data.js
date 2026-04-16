879b2191b89ad9145dd19a2bfadeb63e946acb9e
amvazq <amvazq96@gmail.com>
2026-04-16 07:20:36 +0000

    Deploying to gh-pages from @ amvazq/amvazq.github.io@ad348b55f1547400d25982b53a0827b8bbca3e4c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -457,12 +457,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-this-website-was-created-sparkles-smile",
+          section: "News",},{id: "news-this-website-was-created-sparkles-smile",
           title: 'This website was created! :sparkles: :smile:',
           description: "",
           section: "News",},{id: "projects-project-1",
